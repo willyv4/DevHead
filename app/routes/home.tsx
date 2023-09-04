523feae8b4e912b1170969b5b486ff5912cacf3f
William Valadez <billy.valadez@icloud.com>
2023-09-04 02:41:28 -0400

    added styles tp front page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,30 @@
+import { InboxIcon, TrashIcon, UsersIcon } from "@heroicons/react/24/outline";
 import { Link } from "@remix-run/react";
+import screenshot from "../../public/screen_shot.png";
+
+const features = [
+	{
+		name: "Stream line a portfolio",
+		description:
+			"Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.",
+		href: "#",
+		icon: InboxIcon,
+	},
+	{
+		name: "Connect GitHub",
+		description:
+			"Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.",
+		href: "#",
+		icon: UsersIcon,
+	},
+	{
+		name: "Connect LeetCode",
+		description:
+			"Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.",
+		href: "#",
+		icon: TrashIcon,
+	},
+];
 
 export default function Example() {
 	return (
@@ -48,7 +74,7 @@ export default function Example() {
 							</div>
 						</div>
 						<img
-							src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+							src={screenshot}
 							alt="App screenshot"
 							width={2432}
 							height={1442}
@@ -67,6 +93,42 @@ export default function Example() {
 								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
 						}}
 					/>
+				</div>
+			</div>
+
+			{/* features */}
+
+			<div className="-mt-24">
+				<div className="mx-auto max-w-7xl px-6 lg:px-8">
+					<div className="mx-auto max-w-2xl lg:mx-0">
+						<h2 className="text-3xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+							Streamline Your Portfolio Site.
+						</h2>
+						<p className="mt-6 text-lg leading-8 text-gray-600">
+							Unlock your potential with DevHead. Showcase your skills
+							effortlessly and make your mark in the world of tech.
+						</p>
+					</div>
+					<div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+						<dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+							{features.map((feature) => (
+								<div key={feature.name} className="flex flex-col">
+									<dt className="text-base font-semibold leading-7 text-gray-900">
+										<div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+											<feature.icon
+												className="h-6 w-6 text-white"
+												aria-hidden="true"
+											/>
+										</div>
+										{feature.name}
+									</dt>
+									<dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-400">
+										<p className="flex-auto">{feature.description}</p>
+									</dd>
+								</div>
+							))}
+						</dl>
+					</div>
 				</div>
 			</div>
 		</div>
