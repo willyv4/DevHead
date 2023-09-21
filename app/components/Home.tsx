c799c5130e78a4dd7561e1959df769f1cb5e6091
William Valadez <billy.valadez@icloud.com>
2023-09-21 17:58:32 -0400

    .

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -45,7 +45,7 @@ const Home = () => {
 				<div className="py-24 sm:py-32 lg:pb-40">
 					<div className="mx-auto max-w-7xl px-6 lg:px-8">
 						<div className="mx-auto max-w-2xl text-center">
-							<div className="ml-20 sm:ml-24 text-4xl font-bold tracking-tight text-white sm:text-6xl flex flex-row">
+							<div className="ml-6 sm:ml-24 text-4xl font-bold tracking-tight text-white sm:text-6xl flex flex-row">
 								DevHead:{" "}
 								<div className="ml-2">
 									<Typewriter
