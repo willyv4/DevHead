5d21ca7246848d960a3c42e97309795045f6fac6
William Valadez <billy.valadez@icloud.com>
2025-03-26 20:08:00 -0600

    little bit of logging to debug prod

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://ymkqhqoykdqczyvavbyy.supabase.co";
 const supabaseKey = process.env.DEVHEAD_DB_API_KEY ?? "";
+console.log("🔍 supabaseKey", supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 export { supabase };
--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,17 @@
 import { ClerkApp, useUser, V2_ClerkErrorBoundary } from "@clerk/remix";
 import type {
-	LinksFunction,
-	LoaderFunction,
-	V2_MetaFunction,
+  LinksFunction,
+  LoaderFunction,
+  V2_MetaFunction,
 } from "@vercel/remix";
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useLoaderData,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 import { rootAuthLoader } from "@clerk/remix/ssr.server";
@@ -23,55 +23,58 @@ import logo from "../public/devhead_logo.png";
 
 export const ErrorBoundary = V2_ClerkErrorBoundary();
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: stylesheet },
-	{
-		rel: "icon",
-		href: logo,
-		type: "image/png",
-	},
+  { rel: "stylesheet", href: stylesheet },
+  {
+    rel: "icon",
+    href: logo,
+    type: "image/png",
+  },
 ];
 
 export const meta: V2_MetaFunction = () => [{ title: "DevHead" }];
 export const loader: LoaderFunction = (args) => {
-	return rootAuthLoader(
-		args,
-		async ({ request }) => {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const { userId, sessionId, getToken } = request.auth;
-			const user = await User.getUserById(userId ?? "");
-			let currUser = {};
-			if (user && user) currUser = user;
-			// ("Root loader auth:", { userId, sessionId, getToken });
-			return { currUser: currUser, userId: userId };
-		},
-		{
-			loadUser: true,
-		}
-	);
+  return rootAuthLoader(
+    args,
+    async ({ request }) => {
+      const { userId } = request.auth;
+      const user = await User.getUserById(userId ?? "");
+      let currUser = {};
+      if (user && user) currUser = user;
+
+      return { currUser: currUser, userId: userId };
+    },
+    {
+      loadUser: true,
+    }
+  );
 };
 
 function App() {
-	const { user } = useUser();
-	const { currUser } = useLoaderData();
-	return (
-		<html lang="en" className="bg-gray-900">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				<NavBar currUser={currUser} userId={user?.id} />
-				<Outlet />
-				<Footer />
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-				<Analytics />
-			</body>
-		</html>
-	);
+  const { user } = useUser();
+  const { currUser } = useLoaderData();
+
+  console.log("🔍 currUser", currUser);
+  console.log("🔍 user", user);
+
+  return (
+    <html lang="en" className="bg-gray-900">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <NavBar currUser={currUser} userId={user?.id} />
+        <Outlet />
+        <Footer />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <Analytics />
+      </body>
+    </html>
+  );
 }
 
 export default ClerkApp(App);
